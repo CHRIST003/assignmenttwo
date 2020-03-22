d482c6086e16e5baab41ab3eac2ed5efd0ae8c7a
CHRIST003 <christnganga06@gmail.com>
2020-03-22 14:36:59 +0200

    Initial commit

---
+++ src/test/java/ac/za/cput/AppTest.java
@@ -0,0 +1,66 @@
+package ac.za.cput;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+
+    private App mathRules;
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+        mathRules = new App();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void QuantityEquality() {
+        int result = mathRules.Quantity(1, 0, 0);
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void QuantityIdentity() {
+        int result = mathRules.Quantity(1, 1, 1);
+        Assert.assertSame(3, result);
+    }
+    @Test
+    public void QuantityFail() {
+        Assert.fail("Sorry the test has failed (**)");
+    }
+
+    @Test (timeout = 130)
+    public void QuantityTimeout() {
+        int result = mathRules.Quantity(0, 1, 0);
+        Assert.assertSame(1, result);
+    }
+
+    @Ignore
+    @Test
+    public void volumeDisable() {
+        int result = mathRules.Quantity(1, 1, 1);
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void main() {
+    }
+}
